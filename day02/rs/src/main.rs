991edc0c019df9c837f837012193a18ed026b6d6
kintrix007 <boldi.koller@proton.me>
2023-12-02 11:52:14 +0100

    feat: Implemented day 2 part 1 in Rust

--- day02/rs/src/main.rs
+++ day02/rs/src/main.rs
@@ -7,14 +7,52 @@ struct CubeSet {
     blue: u32,
 }
 
+const LIMIT: CubeSet = CubeSet {
+    red: 12,
+    green: 13,
+    blue: 14,
+};
+
 fn main() {
     let cont = fs::read_to_string("input").expect("File not found");
     let lines: Vec<_> = cont.lines().collect();
-    let games = parse_games(lines);
-    println!("{:?}", games);
+    let games = parse_games(&lines);
+    println!("Part 1: {}", solve1(&games));
 }
 
-fn parse_games<'a>(lines: Vec<&'a str>) -> Vec<Vec<CubeSet>> {
+fn solve1(games: &Vec<Vec<CubeSet>>) -> usize {
+    games
+        .iter()
+        .enumerate()
+        .map(|(i, x)| (i, get_max_cubes(x)))
+        .filter(|(_, x)| is_valid(x))
+        .map(|(i, _)| i + 1)
+        .sum()
+}
+
+fn is_valid(cube_set: &CubeSet) -> bool {
+    return cube_set.red <= LIMIT.red
+        && cube_set.blue <= LIMIT.blue
+        && cube_set.green <= LIMIT.green;
+}
+
+fn get_max_cubes(game: &Vec<CubeSet>) -> CubeSet {
+    let mut max = CubeSet {
+        red: 0,
+        green: 0,
+        blue: 0,
+    };
+
+    for set in game {
+        max.red = std::cmp::max(max.red, set.red);
+        max.green = std::cmp::max(max.green, set.green);
+        max.blue = std::cmp::max(max.blue, set.blue);
+    }
+
+    max
+}
+
+fn parse_games<'a>(lines: &Vec<&'a str>) -> Vec<Vec<CubeSet>> {
     lines
         .iter()
         .map(|ln| {
