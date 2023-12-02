f30edbaeb35d642c638fb95947f54e118320cdd7
kintrix007 <boldi.koller@proton.me>
2023-12-02 07:38:41 +0100

    fix: Pin down day 1 haskell shell pkgs version

--- day01/hs/shell.nix
+++ day01/hs/shell.nix
@@ -1,4 +1,7 @@
-{ pkgs ? import <nixpkgs> {} }:
+let
+  url = "https://github.com/NixOS/nixpkgs/archive/d1c3fea7ecbed758168787fe4e4a3157e52bc808.tar.gz";
+in
+{ pkgs ? import (fetchTarball url) { } }:
 
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
