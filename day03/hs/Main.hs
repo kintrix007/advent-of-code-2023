f38ad574b41e5cf40668264c9b44a7f20419f05f
kintrix007 <boldi.koller@proton.me>
2023-12-03 11:44:24 +0100

    feat: Solve day 3 part 1

--- day03/hs/Main.hs
+++ day03/hs/Main.hs
@@ -1,10 +1,75 @@
 #!/usr/bin/env runhaskell
+import           Control.Exception (assert)
+import           Data.Char         (isDigit, isSymbol)
+import           Data.Function     ((&))
+import           Debug.Trace       (trace)
+
+data Pos = Pos !Int !Int deriving (Eq, Show)
+
+data Symbol
+  = Symbol
+    !Pos -- ^ Position of the Symbol
+    !Char -- ^ The symbol itself
+  deriving (Eq, Show)
+
+data Number
+  = Number
+    !Pos -- ^ Position of the number
+    !Int -- ^ Width of the number
+    !Int -- ^ The number itself
+  deriving (Eq, Show)
+
+data Schematic = Schematic [Symbol] [Number] deriving (Eq, Show)
+
+instance Semigroup Schematic where
+  Schematic ss1 ns1 <> Schematic ss2 ns2 = Schematic (ss1 <> ss2) (ns1 <> ns2)
+
+instance Monoid Schematic where
+  mempty = Schematic [] []
 
 main = do
   ls <- lines <$> readFile "input"
-  print ls
+  let schematic = parse ls
+  -- print schematic
 
-  -- putStr "Part 1: "
-  -- print $ solve1 _
+  putStr "Part 1: "
+  print $ part1 schematic
   -- putStr "Part 2: "
   -- print $ solve2 _
+
+part1 :: Schematic -> Int
+part1 (Schematic ss ns) = sum $ map (\(Number _ _ n) -> n) a
+  where
+    a = ns
+      & filter (\(Number pos w n) -> any (\(Symbol p _) -> aabb p pos w) ss)
+
+aabb :: Pos -> Pos -> Int -> Bool
+aabb (Pos cx cy) (Pos nx ny) nw =
+  cx >= tlx && cy >= tly && cx <= brx && cy <= bry
+  where
+    tlx = nx - 1
+    tly = ny - 1
+    brx = nx + nw
+    bry = ny + 1
+
+parse :: [String] -> Schematic
+parse lines = mconcat $ zipWith parseLine [0..] $ map (zip [0..]) lines
+
+parseLine :: Int -> [(Int, Char)] -> Schematic
+parseLine y [] = mempty
+parseLine y line = parseToken y token <> parseLine y rest
+  where
+    (token, rest) = span (cond . snd) thing
+    thing = dropWhile ((== '.') . snd) line
+    cond = case thing of
+      [] -> const False
+      (_, ch):_ -> if isDigit ch
+        then isDigit
+        else (\x -> x /= '.' && not (isDigit x))
+
+parseToken :: Int -> [(Int, Char)] -> Schematic
+parseToken y [] = Schematic [] []
+parseToken y [(x, c)] | not $ isDigit c = Schematic [Symbol (Pos x y) c] []
+parseToken y ss = Schematic [] [Number (Pos x y) (length cs) (read cs)]
+  where
+    (x:_, cs) = unzip ss
