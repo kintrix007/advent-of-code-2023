df0330290885f564c53b92e4f86fda8ca7e401d7
kintrix007 <boldi.koller@proton.me>
2023-12-05 17:04:07 +0100

    chore: Switch to Stack instead of Haskell Script to be able to add dependencies

--- day05/hs/shell.nix
+++ day05/hs/shell.nix
@@ -1,14 +1,16 @@
 let
-  url = "https://github.com/NixOS/nixpkgs/archive/8cad3dbe48029cb9def5cdb2409a6c80d3acfe2e.tar.gz";
+  url = "https://github.com/NixOS/nixpkgs/archive/70475eb9dd8e0a3b7138cf829f96cb404fa1d94b.tar.gz";
 in
 { pkgs ? import (fetchTarball url) { } }:
 
 pkgs.mkShell {
   nativeBuildInputs = with pkgs; [
-    haskell.compiler.ghc8107
-    (haskell-language-server.override {
-      supportedGhcVersions = [ "8107" ];
-    })
-    # haskellPackages.stack
+    # haskell.compiler.ghc8107
+    # (haskell-language-server.override {
+    #   supportedGhcVersions = [ "8107" ];
+    # })
+    ghc
+    haskell-language-server
+    haskellPackages.stack
   ];
 }
